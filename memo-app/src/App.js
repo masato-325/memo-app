b0055704db1338e2976ac3c121a55998c31875e7
masato <maaton325@gmail.com>
2024-02-04 22:39:44 +0900

    メモ作成フォームの追加

--- memo-app/src/App.js
+++ memo-app/src/App.js
@@ -4,10 +4,17 @@ import MemoList from './MemoList';
 
 
 function App() {
+  const [memos, setMemos] = useState([]); // メモの初期値は空の配列
+
+  const addMemo = (memo) => {   // メモを追加する関数
+    setMemos([...memos, memo]); // 新しいメモを追加
+  };
+
   return (
     <div className="App">
       <h1>メモアプリ</h1>
-      <MemoInput/>
+      <MemoInput onAddMemo={addMemo}/>  
+      <MemoList memos={memos}/>
 
     </div>
   );
